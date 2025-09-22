3f3afd7f688a8c7a973ea521456828d201be1c3a
macrespo42 <macrespo@student.42.fr>
2025-09-22 15:32:37 +0200

    fix: parse duration correctly

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -20,15 +20,27 @@ async function scrapeFeed() {
   }
 }
 
-function parseDuration(durationStr: string): number {
+export function parseDuration(durationStr: string) {
   const regex = /^(\d+)(ms|s|m|h)$/;
   const match = durationStr.match(regex);
-  if (!match || match.length < 1) {
-    throw new Error("Invalid interval given");
-  }
+  if (!match) return;
 
-  console.log(`Collecting feed every ${durationStr}`);
-  return Number(match[1]);
+  if (match.length !== 3) return;
+
+  const value = parseInt(match[1], 10);
+  const unit = match[2];
+  switch (unit) {
+    case "ms":
+      return value;
+    case "s":
+      return value * 1000;
+    case "m":
+      return value * 60 * 1000;
+    case "h":
+      return value * 60 * 60 * 1000;
+    default:
+      return;
+  }
 }
 
 export async function handleAgg(_: string, ...args: string[]) {
